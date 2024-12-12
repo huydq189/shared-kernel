021a6a4807e3fd9dc192af2e6d435c39761ffaec
huydq189 <salt.coffee189@gmail.com>
2024-12-12 22:20:03 +0700

    WIP: add domain event base class

--- src/interfaces/has-domain-event.ts
+++ src/interfaces/has-domain-event.ts
@@ -1,7 +1,7 @@
 export abstract class DomainEventBase {
-  public readonly dateOccurred: Date;
+  public readonly dateOccurred: Date
 
   constructor() {
-    this.dateOccurred = new Date(); // Equivalent to DateTime.UtcNow in C#
+    this.dateOccurred = new Date()
   }
 }
